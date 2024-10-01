46c4dac7de0ffa2b84f6d48424aa4e568707c70a
Wayne <lucaswayne779@gmail.com>
2024-10-01 09:02:02 -0300

    a

---
+++ departmentservice/src/main/java/net/javaguides/departmentservice/entity/Department.java
@@ -0,0 +1,25 @@
+package net.javaguides.departmentservice.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import net.javaguides.departmentservice.DepartmentserviceApplication;
+import org.springframework.boot.SpringApplication;
+
+@Entity
+@Table(name = "departments")
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+public class Department {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String departmentName;
+    private String departmentAddress;
+    private String departmentCode;
+
+}
